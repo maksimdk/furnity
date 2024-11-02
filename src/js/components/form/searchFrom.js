04b328e9bfd5ff28fc2bcba122754135a88dd4fe
maksim <maksimkot.work@gmail.com>
2024-11-02 22:11:20 +0300

    Added search furnity

--- src/js/components/form/searchFrom.js
+++ src/js/components/form/searchFrom.js
@@ -1,6 +1,9 @@
 export const searchForm = () => {
   const form = document.forms.main;
   const formInput = document.querySelector('.main-form__input');
+  const formButton = document.querySelector('.main-form__button');
+  const resultBlock = document.querySelector('.header__result');
+  const resultProducts = document.querySelector('.result__products');
 
   const formInputPlaceholder = formInput.placeholder;
   formInput.addEventListener('focus', event => {
@@ -12,7 +15,54 @@ export const searchForm = () => {
     form.style.border = '2px solid var(--text-main-color)';
   });
 
-  formInput.addEventListener('input', event => {
-    console.log(formInput.value);
+  if (resultProducts.children.length === 0) {
+    resultBlock.style.display = 'none';
+  }
+
+  form.addEventListener('submit', event => {
+    fetch(`https://6696e3d50312447373c404b2.mockapi.io/offers/product?search=${formInput.value}`)
+      .then(response => {
+        if (response.status === 404) {
+          alert('Введите корректные данные!');
+          return;
+        }
+        if (!response.ok) {
+          throw new Error(`${response.status} - ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        if (json.length === 0) {
+          alert('Введите корректные данные!');
+          return;
+        }
+
+        resultBlock.style.display = 'block';
+
+        json.forEach(item => {
+          resultProducts.innerHTML += `
+										<li class="result__product">
+                      <div class="product__content">
+                        <div class="product__image">
+                          <img
+                            src="${item.imageUrl}"
+                            alt="${item.name}" />
+                        </div>
+                        <div class="product__title">
+                          <p class="product__name">${item.name}</p>
+                          <p class="product__category">${item.type}</p>
+                        </div>
+                      </div>
+                      <p class="product__price">${item.currency} ${item.price}</p>
+                    </li>
+					`;
+        });
+      })
+      .catch(err => console.log(err.message));
+    event.preventDefault();
   });
+
+  // Add block clearing
+  resultProducts.innerHTML = '';
+  // Add block clearing
 };
