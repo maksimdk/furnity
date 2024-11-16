70b6647b635292fb4fffd721d5960e49594a0ec4
maksim <maksimkot.work@gmail.com>
2024-11-16 18:12:43 +0300

    Added show card

--- docs/src/js/main.js
+++ docs/src/js/main.js
@@ -2,3 +2,8 @@ import { showHeader } from './sections/header/showHeader.js';
 import { showOffers } from './sections/offers/showOffers.js';
 import { showCollections } from './sections/collections/showCollections.js';
 import { showReviews } from './sections/reviews/showReviews.js';
+
+//===================
+import { showCard } from './components/products/showCard.js';
+showCard();
+//===================
